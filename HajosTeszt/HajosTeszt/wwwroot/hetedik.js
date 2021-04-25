f2436c53935b9e8f1451e663b4d06eae83352ae9
Botond Kovács <botond.kovacs@stud.uni-corvinus.hu>
2021-04-25 14:12:38 +0200

    gyak9

--- HajosTeszt/HajosTeszt/wwwroot/hetedik.js
+++ HajosTeszt/HajosTeszt/wwwroot/hetedik.js
@@ -1,61 +1,77 @@
-﻿var kérdések;
-var k = 0;
+﻿
+
+var kérdések;
+var k = 1;
 var kérdés
 var jóVálasz
+var kép
 
 window.onload = () => {
     letöltés();
-
 }
 function letöltés() {
-    fetch('/questions.json')
+    fetch('/questions/1')
         .then(response => response.json())
-        .then(data => letöltésBefejeződött(data));
-}
-
-function letöltésBefejeződött(d) {
-    console.log("Sikeres letöltés")
-    console.log(d)
-    kérdések = d;
-    kérdésMegjelenítés(k);
+        .then(data => kérdésMegjelenítés(data)
+        );
 }
 
 function kérdésMegjelenítés(kérdés) {
     katt();
+    document.getElementById("kép1").src = "";
     document.getElementById("válasz1").classList.remove("jó");
     document.getElementById("válasz2").classList.remove("jó");
     document.getElementById("válasz3").classList.remove("jó");
     document.getElementById("válasz1").classList.remove("rossz");
     document.getElementById("válasz2").classList.remove("rossz");
     document.getElementById("válasz3").classList.remove("rossz");
-    document.getElementById("kérdés_szöveg").innerHTML = kérdések[kérdés].questionText;
-    document.getElementById("válasz1").innerHTML = kérdések[kérdés].answer1;
-    document.getElementById("válasz2").innerHTML = kérdések[kérdés].answer2;
-    document.getElementById("válasz3").innerHTML = kérdések[kérdés].answer3;
-    document.getElementById("kép1").src = "https://szoft1.comeback.hu/hajo/" + kérdések[kérdés].image;
-    jóVálasz = kérdések[kérdés].correctAnswer;
+    console.log(kérdés);
+    document.getElementById("kérdés_szövege").innerText = kérdés.questionText
+    document.getElementById("válasz1").innerText = kérdés.answer1
+    document.getElementById("válasz2").innerText = kérdés.answer2
+    document.getElementById("válasz3").innerText = kérdés.answer3
+
+    kép = kérdés.image;
+    if (kép != "") document.getElementById("kép1").src = "https://szoft1.comeback.hu/hajo/" + kép;
+
+    jóVálasz = kérdés.correctAnswer;
+    console.log(jóVálasz)
 }
+function kérdésBetöltés(id) {
+    fetch(`/questions/${id}`)
+        .then(response => {
+            if (!response.ok) {
+                console.error(`Hibás válasz: ${response.status}`)
+            }
+            else {
+                return response.json()
+            }
+        })
+        .then(data => kérdésMegjelenítés(data));
+}    
+
 function vissza() {
     if (k == 0) {
-        k = kérdések.length - 1
+        k = 859
     }
     else {
         k--
     }
-    kérdésMegjelenítés(k)
+    kérdésBetöltés(k)
 }
 function előre() {
-    if (k == kérdések.length - 1) {
-        k = 0
+    if (k == 859) {
+        k = 1
     }
     else {
         k++
     }
-    kérdésMegjelenítés(k)
+    kérdésBetöltés(k)
 }
 
+
+
 function katt() {
-    
     document.getElementById("válasz1").classList.add("kattintható");
     document.getElementById("válasz2").classList.add("kattintható");
     document.getElementById("válasz3").classList.add("kattintható");
@@ -71,6 +87,8 @@ function válaszSzínezés1() {
     nemKatt();
     if (jóVálasz == 1) {
         document.getElementById("válasz1").classList.add("jó")
+        document.getElementById("válasz2").classList.add("rossz")
+        document.getElementById("válasz3").classList.add("rossz")
     }
     else document.getElementById("válasz1").classList.add("rossz")
 }
@@ -79,6 +97,8 @@ function válaszSzínezés2() {
     nemKatt();
     if (jóVálasz == 2) {
         document.getElementById("válasz2").classList.add("jó")
+        document.getElementById("válasz1").classList.add("rossz")
+        document.getElementById("válasz3").classList.add("rossz")
     }
     else document.getElementById("válasz2").classList.add("rossz")
 }
@@ -87,6 +107,8 @@ function válaszSzínezés3() {
     nemKatt();
     if (jóVálasz == 3) {
         document.getElementById("válasz3").classList.add("jó")
+        document.getElementById("válasz2").classList.add("rossz")
+        document.getElementById("válasz1").classList.add("rossz")
     }
     else document.getElementById("válasz3").classList.add("rossz")
 }
